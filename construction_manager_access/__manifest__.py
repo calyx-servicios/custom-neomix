1232a59dcbce316712df3a32145d4c8261f155e3
paradisocrist <crisparadiso_@hotmail.com>
2021-03-09 11:11:33 -0300

    [ADD][construction_manager_access]new group construction manager

---
+++ construction_manager_access/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Construction Manager Access",
+    'summary': """ New group construction manager""",
+    'description': """ """,
+    'author': "Calyx",
+    'website': "http://www.calyxservicios.com.ar",
+    'category': 'Tools',
+    'version': '11.0.1.0.0',
+    'depends': [],
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+    ],
+    'demo': [
+    ],
+}
