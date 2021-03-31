8bfc9e3738f06ac1c7106a6b08bf4ea5f315f0f1
paradisocrist <crisparadiso_@hotmail.com>
2021-03-31 07:54:02 -0300

    [ADD][l10n_ar_aeroo_invoice_order_number]

---
+++ report_aeroo_extra_function/__manifest__.py
@@ -0,0 +1,43 @@
+##############################################################################
+#
+#    Copyright (C) 2015  ADHOC SA  (http://www.adhoc.com.ar)
+#    All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Argentinian Like Electronic Invoice Aeroo Report add Extra Function',
+    'version': '11.0.1.3.0',
+    'category': 'Localization/Argentina',
+    'sequence': 14,
+    'author': 'Saitam',
+    'website': '',
+    'license': 'AGPL-3',
+    'summary': '',
+    'depends': [
+        # suponemos que si instalas este queres el comun tmb
+        'report_aeroo',
+    ],
+    'external_dependencies': {
+    },
+    'data': [
+        
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
