afdf435fff42ef6a72f209f3947ff20a378fec1f
Esteban Suarez <estebansamsuarez20@gmail.com>
2025-07-08 08:03:14 -0300

    [UPD][l10n_ar_afip_ws_ARCA]

--- l10n_ar_afip_ws_ARCA/models/account_invoice.py
+++ l10n_ar_afip_ws_ARCA/models/account_invoice.py
@@ -9,6 +9,7 @@ import json
 import sys
 import traceback
 from datetime import datetime
+from odoo.tools.misc import formatLang
 _logger = logging.getLogger(__name__)
 
 try:
@@ -23,6 +24,42 @@ except ImportError:
 
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
+
+    def get_tax_detail_ar(self):
+        involved_tax_group_ids = []
+        for line in self.tax_line_ids:
+            involved_tax_group_ids.append(line.tax_id.tax_group_id.id)
+        involved_tax_groups = self.env['account.tax.group'].browse(involved_tax_group_ids)
+        nat_tax_groups = involved_tax_groups.filtered(lambda tax_group: tax_group.afip_code in (1, 4))
+        vat_tax_groups = involved_tax_groups.filtered(lambda tax_group: tax_group.afip_code != 0)
+
+        # RG 5614/2024: Show ARCA VAT and Other National Internal Taxes
+        if self.journal_document_type_id.document_type_id.code in ['6', '7', '8']:
+            # Prepare the subtotals to show in the report
+            currency_symbol = self.currency_id.symbol
+            detail_info = {}
+
+            for line in self.tax_line_ids:
+                if line.tax_id.tax_group_id in nat_tax_groups:
+                    key = 'other_taxes'
+                    name = _("Other National Ind. Taxes %s") % currency_symbol
+                elif line.tax_id.tax_group_id in vat_tax_groups:
+                    key = 'vat_taxes'
+                    name = _("VAT Content %s") % currency_symbol
+                else:
+                    continue
+
+                if key not in detail_info:
+                    if line.amount_total != 0.0:
+                        detail_info[key] = {"name": name, "tax_amount": line.amount_total}
+                else:
+                    detail_info[key]["tax_amount"] += line.amount_total
+
+            # Format the amounts to show in the report
+            for _item, values in detail_info.items():
+                values["formatted_amount_tax"] = formatLang(self.env, values["tax_amount"])
+
+            return list(detail_info.values())
 
     l10n_ar_payment_foreign_currency = fields.Selection(
         [("S", "Yes"), ("N", "No")],
@@ -639,4 +676,3 @@ class AccountInvoice(models.Model):
 
     # def pyafipws_get_currency_rate(self, ws):
     #     return ws.ParamGetCotizacion(self.currency_id.l10n_ar_afip_code)
-    
